309e3861ee678c2e5a538daddcae50049b5b4cb1
Tampoka <goal.thanks@gmail.com>
2022-06-10 20:31:35 +0900

    Add posts to props.

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,19 +1,21 @@
-import {Grid,Title} from './styles'
+import {Grid, Title} from './styles'
+import {Post as PostType} from '../../shared/types';
 import {Post} from '../Post';
 
 type SectionProps = {
     title: string
+    posts: PostType[]
 }
 
 
-export  const Section = ({ title }: SectionProps) => {
+export const Section = ({title}: SectionProps) => {
     return (
         <section>
             <Title>{title}</Title>
             <Grid>
-                <Post />
-                <Post />
-                <Post />
+                <Post/>
+                <Post/>
+                <Post/>
             </Grid>
         </section>
     )
