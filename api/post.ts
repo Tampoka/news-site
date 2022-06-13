fbb0accba033ff47759d241b87896bdffd1cc17d
Tampoka <goal.thanks@gmail.com>
2022-06-13 22:29:59 +0900

    Add getStaticPaths.

--- api/post.ts
+++ api/post.ts
@@ -1,7 +1,7 @@
 import {EntityId} from '../shared/types';
 import {config} from './config';
 
-export async function fetchPosts(id: EntityId) {
+export async function fetchPost(id: EntityId) {
     const res = await fetch(`${config.baseUrl}/posts/${id}`)
     return await res.json()
 }
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,11 +1,31 @@
-import { useRouter } from "next/router"
+import {useRouter} from "next/router";
+import {GetStaticProps} from 'next';
+import {Post as PostType} from '../../shared/types';
+import {postPaths as paths} from '../../shared/staticPaths'
+import {fetchPost} from '../../api/post';
 
-const Post = () => {
-    const { pathname, query } = useRouter()
+type PostProps = {
+    post: PostType
+}
+
+export const getStaticProps: GetStaticProps<PostProps> = async ({
+                                                                    params
+                                                                }) => {
+    if (typeof params?.id !== "string") throw new Error("Unexpected id")
+    const post = await fetchPost(params?.id)
+    return {props: {post}}
+}
+
+export async function getStaticPaths() {
+    return {paths, fallback: true}
+}
+
+const Post = ({post}: PostProps) => {
+    const {pathname, query} = useRouter()
 
     return (
         <div>
-            Pathname: {pathname};<br />
+            Pathname: {pathname};<br/>
             Post Id: {query.id}.
         </div>
     )
