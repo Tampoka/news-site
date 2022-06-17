e5b3750a13f385d7c39bd13c3e4145f3e9ecbc8e
Tampoka <goal.thanks@gmail.com>
2022-06-17 23:40:58 +0900

    Add static paths.

--- shared/staticPaths.ts
+++ shared/staticPaths.ts
@@ -1,4 +1,4 @@
-import {EntityId} from './types';
+import {Category, EntityId} from './types';
 
 type PostStaticParams = {
     id: EntityId
@@ -11,4 +11,21 @@ export const postPaths: PostStaticPath[] = staticPostsIdList.map(
     (id) => ({
         params: { id: String(id) }
     })
+)
+
+type CategoryStaticParams = {
+    id: Category
+}
+
+type CategoryStaticPath = {
+    params: CategoryStaticParams
+}
+
+const categoriesToPreRender: Category[] = [
+    "Science",
+    "Technology",
+    "Arts"
+]
+export const categoryPaths: CategoryStaticPath[] = categoriesToPreRender.map(
+    (category) => ({ params: { id: category } })
 )
