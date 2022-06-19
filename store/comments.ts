c1f5bceba4e61b1ca3f93fed40718013b9d1e36d
Tampoka <goal.thanks@gmail.com>
2022-06-19 22:58:29 +0900

    Add comments reducer.

--- store/comments.ts
+++ store/comments.ts
@@ -1,1 +1,28 @@
+import { Comment } from "../shared/types"
+import {AnyAction} from 'redux';
+import {HydrateAction} from './hydrate';
+import {HYDRATE} from 'next-redux-wrapper';
+
+export const UPDATE_COMMENTS_ACTION = "UPDATE_COMMENTS"
+
+export interface UpdateCommentsAction extends AnyAction {
+    type: typeof UPDATE_COMMENTS_ACTION
+    comments: Comment[]
+}
+
 export type CommentsState = Comment[]
+type CommentsAction = HydrateAction | UpdateCommentsAction
+
+export const comments = (
+    state: CommentsState = [],
+    action: CommentsAction
+) => {
+    switch (action.type) {
+        case HYDRATE:
+            return action.payload?.comments ?? []
+        case UPDATE_COMMENTS_ACTION:
+            return action.comments
+        default:
+            return state
+    }
+}
