4d4861c87cb5d920fab7dab257135508a854dedf
Tampoka <goal.thanks@gmail.com>
2022-06-19 22:43:36 +0900

    Add store.

--- store/index.ts
+++ store/index.ts
@@ -1,7 +1,17 @@
 import {CommentsState} from './comments';
 import {PostState} from './post';
+import {combineReducers} from 'redux';
+import { legacy_createStore as createStore} from 'redux';
+import {createWrapper, MakeStore} from 'next-redux-wrapper';
 
 export type State={
     post:PostState
     comments:CommentsState
 }
+
+const combineReducer=combineReducers({post,comments})
+const makeStore:MakeStore<State>=()=>createStore(combineReducer)
+
+export const store = createWrapper<State>(makeStore, {
+    debug: true
+})
