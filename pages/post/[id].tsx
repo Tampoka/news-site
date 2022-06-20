705cbbc810caa8e3b9ad3c7437739e246d7cd6ae
Tampoka <goal.thanks@gmail.com>
2022-06-20 13:21:11 +0900

    Fix getServerSideProps.

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,5 +1,5 @@
 import {useRouter} from "next/router";
-import {GetServerSideProps, GetStaticProps} from 'next';
+import {GetServerSideProps, GetStaticProps, NextPage} from 'next';
 import {Post as PostType, Comment} from '../../shared/types';
 import {postPaths as paths} from '../../shared/staticPaths'
 import {fetchPost} from '../../api/post';
@@ -7,6 +7,10 @@ import {PostBody} from '../../components/Post/PostBody';
 import {Loader} from '../../components/Loader';
 import {fetchComments} from '../../api/comments';
 import {Comments} from '../../components/Comments/Comments';
+import {State, store} from '../../store';
+import {UPDATE_COMMENTS_ACTION} from '../../store/comments';
+import {UPDATE_POST_ACTION} from '../../store/post';
+import {useSelector} from 'react-redux';
 
 type PostProps = {
     post: PostType
@@ -21,23 +25,29 @@ type PostProps = {
     return {props: {post}}
 }*/
 
-export const getServerSideProps: GetServerSideProps<PostProps> = async ({
-                                                                            params
-                                                                        }) => {
-    if (typeof params?.id !== "string") throw new Error("Unexpected id")
-    const post = await fetchPost(params.id)
-    const comments = await fetchComments(params.id)
-    return {props: {post, comments}}
-}
+export const getServerSideProps = store.getServerSideProps(
+    (store) =>
+        async ({params}) => {
+            if (typeof params?.id !== "string")
+                throw new Error("Unexpected id")
+
+            const comments = await fetchComments(params.id) as Comment[]
+            const post = await fetchPost(params.id) as PostType
+
+            store.dispatch({type: UPDATE_POST_ACTION, post})
+            store.dispatch({type: UPDATE_COMMENTS_ACTION, comments})
+            return {props: {post, comments}};
+        }
+)
 
 // export async function getStaticPaths() {
 //     return {paths, fallback: true}
 // }
 
-const Post = ({post, comments}: PostProps) => {
-    const router = useRouter()
+const Post: NextPage = () => {
+    const {post, comments} = useSelector<State, State>(state => state)
 
-    if (router.isFallback) return <Loader/>
+    if (!post) return <Loader/>
     return (
         <>
             <PostBody post={post}/>
