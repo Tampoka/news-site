cfc3e8b6d98117882af54d1c702bf20fe2e7d4ae
Tampoka <goal.thanks@gmail.com>
2022-08-08 23:47:59 +0900

    Fix footer.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
-import { Center } from "../Center"
-import { Container } from "./styles"
+import {Center} from "../Center"
+import {Container} from "./styles"
 
 export const Footer = () => {
     const currentYear = new Date().getFullYear()
     return (
         <Container>
             <Center>
-                <a href="https://newline.co">Newline.co</a> {currentYear}
+                <a href="https://github.com/Tampoka" target="_blank"
+                   rel="noopener noreferrer">Tampoka</a> {currentYear}
             </Center>
         </Container>
     )
