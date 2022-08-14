0002c50c1bf35ecd4d090d035854dc6e695db8be
Tampoka <goal.thanks@gmail.com>
2022-08-14 21:40:46 +0900

    Fix conditional devMode.

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,3 @@
-const devMode = false
-
 export const config = {
-    baseUrl: devMode ? process.env.NEXT_PUBLIC_DEV_BASE_URL : process.env.NEXT_PUBLIC_BASE_URL
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL
 }
